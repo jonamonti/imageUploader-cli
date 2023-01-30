912372a679d4d9729914fe73eac29d01673a2379
Jonathan Monti <jonathanmonti@Jonathans-MacBook-Pro.local>
2023-01-30 17:37:52 -0300

    changes on loader

--- components/Loader.js
+++ components/Loader.js
@@ -1,43 +1,27 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 export default function Loader() {
-  return (
-    <View style={styles.view__Loader}>
-      <View style={styles.view__ContLoader}>
-        <View style={styles.view__TextLoader}>
-          {/* <Text style={styles.titleUploading}>Uploading...</Text> */}
-          <LottieView 
-            autoPlay
-            source={require('../assets/animations/loading.json')}
-            autosize
-            />
-        </View>
-      </View>
-    </View>
-  )
+	return (
+		<View style={styles.viewLoader}>
+			{/* <Text style={styles.titleUploading}>Uploading...</Text> */}
+			<LottieView
+				autoPlay
+				source={require('../assets/animations/loading-bar.json')}
+				autosize
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  view__Loader:{
-    height: '100%',
-    width:'100%',
-    backgroundColor: '#fafafb',
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  view__ContLoader:{
-    height:120,
-    width:350,
-    borderRadius:12,
-    backgroundColor:'#fffffa'
-  },
-  view__TextLoader:{
-    margin:20,
-  },
-  titleUploading:{
-    fontWeight:'400'
-  }
+	viewLoader: {
+		height: '100%',
+		width: '100%',
+		backgroundColor: '#fafafb',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
