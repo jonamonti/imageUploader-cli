b785b30516856998033de4576a365039c705c528
Jonathan Monti <jonathanmonti@Jonathans-MacBook-Pro.local>
2023-01-30 16:55:15 -0300

    adding Lottie pkg and animation to uploadScreen

--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function UploadScreen() {
 	return (
@@ -6,10 +7,22 @@ export default function UploadScreen() {
 			<View style={s.innerContainer}>
 				<View style={s.titleContainer}>
 					<Text style={s.title}>Upload your image</Text>
-					<Text style={s.text}>File should be Jpeg, Png</Text>
+					<Text style={s.text}>File should be Jpeg, Png ...</Text>
 				</View>
-				<View style={s.imageContainer}></View>
-				<View style={s.buttonContainer}></View>
+				<View style={s.centralContainer}>
+					<Pressable style={({ pressed }) => pressed && s.onPress}>
+						<View style={s.animationContainer}>
+							<LottieView
+								source={require('../assets/animations/upload-files.json')}
+								autoPlay
+								autosize
+							/>
+						</View>
+						<View style={s.textContainer}>
+							<Text>Choose a file</Text>
+						</View>
+					</Pressable>
+				</View>
 			</View>
 		</View>
 	);
@@ -31,20 +44,34 @@ const s = StyleSheet.create({
 	},
 	innerContainer: {
 		flex: 1,
+		padding: 24,
+		marginBottom: 24,
 	},
 	titleContainer: {
 		flex: 2,
 		justifyContent: 'center',
 	},
-	imageContainer: {
+	centralContainer: {
 		flex: 6,
+		justifyContent: 'center',
+		alignItems: 'center',
+		// marginBottom: 16,
 		backgroundColor: '#e9ecef',
 		borderWidth: 2,
 		borderStyle: 'dashed',
 		borderColor: 'lightblue',
 		borderRadius: 8,
 	},
-	buttonContainer: { flex: 2, borderWidth: 2 },
+	animationContainer: {
+		flex: 5,
+		height: 200,
+		width: 200,
+	},
+	textContainer: {
+		flex: 2,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 	title: {
 		fontWeight: 'bold',
 		fontSize: 24,
@@ -54,5 +81,8 @@ const s = StyleSheet.create({
 		fontSize: 14,
 		textAlign: 'center',
 		marginVertical: 8,
+	},
+	onPress: {
+		opacity: 0.5,
 	},
 });
