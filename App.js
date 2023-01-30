595a04d25db5ede2ca225e6bde23c0b385af5d0f
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-01-30 17:25:47 -0300

    feat/add_loader

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Loader from './components/Loader';
 import UploadScreen from './screens/UploadScreen';
 
 export default function App() {
 	return (
 		<View style={s.container}>
-			<UploadScreen />
+			{/* <UploadScreen /> */}
+			<Loader />
 			<StatusBar style='auto' />
 		</View>
 	);
