4c868bf7f66361dac7f208f1ccabf00808a6c3f8
Santiago Caballero <saantyc12.caballero@gmail.com>
2023-01-30 18:02:29 -0300

    feat/add_loader

--- components/Loader.js
+++ components/Loader.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import LottieView from 'lottie-react-native';
 
 export default function Loader() {
 	return (
 		<View style={styles.viewLoader}>
-			{/* <Text style={styles.titleUploading}>Uploading...</Text> */}
-			<LottieView
-				autoPlay
-				source={require('../assets/animations/loading-bar.json')}
-				autosize
-			/>
+      <View style={styles.viewLottie}>
+      <Text style={styles.textLoader}>Uploading...</Text>
+        <LottieView
+            autoPlay
+            source={require('../assets/animations/loading-bar.json')}
+            style={styles.img}
+          />
+      </View>
 		</View>
 	);
 }
@@ -24,4 +26,20 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
+  viewLottie:{
+    position:'relative',
+    backgroundColor: '#ffffff',
+    borderRadius:9,
+    height: 120,
+    width:300,
+  },
+  textLoader:{
+    position:'absolute',
+    bottom:60,
+    left:28
+  },
+  img:{
+    width:'100%',
+    marginBottom:30,
+  }
 });
